60680bf9b4d4f96d9eacd5610fa241a1a5d25f6f
Abanoub Nakhla <>
2025-05-27 21:00:15 +0200

    initialize project and connect sqlite db and create subscribe endpoint

---
+++ models/index.js
@@ -0,0 +1,24 @@
+const { Sequelize, DataTypes } = require('sequelize');
+
+// Crea il database SQLite locale
+const sequelize = new Sequelize({
+  dialect: 'sqlite',
+  storage: 'database.sqlite'
+});
+
+// Definisci il modello User con un campo "email"
+const User = sequelize.define('User', {
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      isEmail: true
+    }
+  }
+});
+
+// Sincronizza il modello con il database
+sequelize.sync();
+
+module.exports = { sequelize, User };
---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const { User } = require('./models');
+
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.json()); //Middleware
+app.use(cors())
+
+app.post('/subscribe', async (req, res) => {
+  const { email } = req.body;
+
+  if (!email) {
+    return res.status(400).json({ error: 'Email is required' });
+  }
+
+  try {
+    const newUser = await User.create({ email })
+    res.status(201).json({ message: 'Email saved', user: newUser });
+  } catch (err) {
+    res.status(500).json({ error: 'Failed to save email', details: err.message });
+  }
+});
+
+// Avvia il server
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
