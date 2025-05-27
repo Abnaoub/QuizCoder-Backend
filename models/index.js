63c7ed6d998acae4a927127ad943109383fe8448
Abanoub Nakhla <>
2025-05-27 22:08:29 +0200

    add subscribe api endpoint

--- models/index.js
+++ models/index.js
@@ -1,21 +1,21 @@
-const { Sequelize, DataTypes } = require('sequelize');
+const { Sequelize, DataTypes } = require("sequelize");
 
 // Crea il database SQLite locale
 const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: 'database.sqlite'
+  dialect: "sqlite",
+  storage: "database.sqlite",
 });
 
 // Definisci il modello User con un campo "email"
-const User = sequelize.define('User', {
+const User = sequelize.define("User", {
   email: {
     type: DataTypes.STRING,
     allowNull: false,
     unique: true,
     validate: {
-      isEmail: true
-    }
-  }
+      isEmail: true,
+    },
+  },
 });
 
 // Sincronizza il modello con il database
--- server.js
+++ server.js
@@ -1,26 +1,39 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const { User } = require('./models');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const { User } = require("./models");
 
 const app = express();
 const PORT = 3000;
 
 app.use(bodyParser.json()); //Middleware
-app.use(cors())
+app.use(cors());
 
-app.post('/subscribe', async (req, res) => {
+app.post("/subscribe", async (req, res) => {
   const { email } = req.body;
 
   if (!email) {
-    return res.status(400).json({ error: 'Email is required' });
+    return res.status(400).json({ error: "Email is required" });
   }
 
   try {
-    const newUser = await User.create({ email })
-    res.status(201).json({ message: 'Email saved', user: newUser });
+    const newUser = await User.create({ email });
+    res.status(201).json({ message: "Email saved", user: newUser });
   } catch (err) {
-    res.status(500).json({ error: 'Failed to save email', details: err.message });
+    res
+      .status(500)
+      .json({ error: "Failed to save email", details: err.message });
+  }
+});
+
+app.get("/subscribe", async (req, res) => {
+  try {
+    const allUsers = await User.findAll();
+    res.status(200).json({ message: "All users", subscribedUsers: allUsers });
+  } catch (err) {
+    res
+      .status(500)
+      .json({ error: "Failed to get users", details: err.message });
   }
 });
 
